ad4de4087a199d9979924303f638a70241c09849
Mkayalai01 <mkayalai01@gmail.com>
2024-01-02 13:15:11 +0530

    add exmp file

---
+++ exmp.c
@@ -0,0 +1,54 @@
+
+int addition(int a, int b)
+{
+    printf("\n\n_______Arthimatic________ \n\n");
+    int c;
+    c=a+b;
+    printf("sum is :%d\n\n",c);
+    return 0;
+}
+int subraction (int a, int b)
+{
+
+    int c;
+    c=a-b;
+    printf("sub is :%d\n\n",c);
+    return 0;
+
+}
+int multipication (int a, int b)
+{
+     int c;
+    c=a*b;
+    printf("multiply is :%d\n\n",c);
+    return 0;
+
+}
+int Division (a,b)
+{
+    int D;
+    D=a/b;
+    printf("Division is :%d\n\n",D);
+    return 0;
+}
+
+int assignment(int a, int b)
+
+{
+    printf("_______Assignment________ \n\n");
+    a=b;
+    printf("P value is :%d\n\n",a=b);
+    a+=b;
+    printf("Q value is :%d\n\n",a+=b);
+    a-=b;
+    printf("R value is :%d\n\n",a-=b);
+    a/=b;
+    printf("S value is :%d\n\n",a/=b);
+    a%=b;
+    printf("T value is :%d\n\n",a%=b);
+    a&=b;
+    printf("U values is:%d\n\n",a&=b);
+     a&=6;
+    printf("U1 values is:%d\n\n",a&=6);
+    return 0;
+}
